44240267acf03dd12bd3e336a162265606704826
Nazar Suhovich <n.suhovich@gmail.com>
2019-07-05 18:42:25 +0300

    Final changes.

--- app/src/main/java/com/github/nazar/batterywidget/MainActivity.java
+++ app/src/main/java/com/github/nazar/batterywidget/MainActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -16,11 +16,14 @@ public class MainActivity extends Activity {
 
     private TextView batteryStatus;
     private TextView batteryLevel;
+    private TextView batteryHealth;
+    private TextView batteryTemperature;
+    private TextView batteryVoltage;
+    private ImageView iconView;
+
     private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.d(TAG, "onReceive: " + intent.getExtras().keySet());
-
             int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
             int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
             int percent = (level * 100) / scale;
@@ -32,6 +35,23 @@ public class MainActivity extends Activity {
             } else {
                 batteryStatus.setText("Idle");
             }
+
+            int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
+            batteryHealth.setText(healthStatus(health));
+
+            int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+            batteryVoltage.setText(String.valueOf(voltage));
+
+            int temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+            float celcius = (temperature / 10f);
+            batteryTemperature.setText(String.format("%.1f", celcius) + "°C");
+
+            int icon = intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL, -1);
+            if (icon != -1) {
+                iconView.setImageResource(icon);
+            } else {
+                iconView.setImageResource(R.drawable.ic_launcher);
+            }
         }
     };
 
@@ -40,13 +60,36 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        iconView = (ImageView) findViewById(R.id.iconView);
         batteryStatus = (TextView) findViewById(R.id.batteryStatus);
         batteryLevel = (TextView) findViewById(R.id.batteryLevel);
+        batteryHealth = (TextView) findViewById(R.id.batteryHealth);
+        batteryTemperature = (TextView) findViewById(R.id.batteryTemperature);
+        batteryVoltage = (TextView) findViewById(R.id.batteryVoltage);
 
         IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
         filter.addAction(Intent.ACTION_POWER_CONNECTED);
         filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         this.registerReceiver(this.mBatInfoReceiver, filter);
+    }
+
+    private String healthStatus(int health) {
+        switch (health) {
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                return "Dead";
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                return "Good";
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                return "Over voltage";
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                return "Overheat";
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                return "Unknown";
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                return "Failure";
+            default:
+                return "Unknown";
+        }
     }
 
     @Override
